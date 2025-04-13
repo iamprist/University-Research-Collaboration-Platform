9614be7def518c3ea68e25fd02c9c688ecbe7376
xxiv-karatz <xxiv.karatz@gmail.com>
2025-04-13 19:38:57 +0200

    Fixed test coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders login options', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+
+  const heading = screen.getByText(/login as/i);
+  expect(heading).toBeInTheDocument();
+
+  const researcherBtn = screen.getByText(/researcher/i);
+  const reviewerBtn = screen.getByText(/reviewer/i);
+  const adminBtn = screen.getByText(/admin/i);
+
+  expect(researcherBtn).toBeInTheDocument();
+  expect(reviewerBtn).toBeInTheDocument();
+  expect(adminBtn).toBeInTheDocument();
 });
