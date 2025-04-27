0767ac10dea3eca465fb40c0e92221b26dc3b32b
iamprist <102292590+iamprist@users.noreply.github.com>
2025-04-27 18:41:38 +0000

    removed problematic test files

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest"
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!axios)/"  // Ensure Axios is transformed
+  ],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  globalSetup: "./__mocks__/firebase.js",  // Optional if you want to globally mock Firebase
+};
