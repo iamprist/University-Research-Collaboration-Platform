286272428d8ebc4d697c42af8aaa5d66d1cc8ade
NkosinathiTshabalala <113860822+NkosinathiTshabalala@users.noreply.github.com>
2025-05-25 22:20:20 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Import Link from React Router
-import Logo from "../assets/Logo.svg";
 import { HiOutlineBars3 } from "react-icons/hi2";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
